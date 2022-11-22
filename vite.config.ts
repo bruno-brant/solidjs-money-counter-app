a9db7819dbc219975b42a4c7e6cf3401c3162b85
Bruno Brant <1890900+bruno-brant@users.noreply.github.com>
2022-11-22 10:04:09 -0300

    chore: move assets to the public directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 import suidPlugin from "@suid/vite-plugin";
-import eslint from 'vite-plugin-eslint'
+import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
-  plugins: [suidPlugin(), solidPlugin(), eslint({ cache: true })],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+	plugins: [suidPlugin(), solidPlugin(), eslint({ cache: true })],
+	server: {
+		port: 3000,
+	},
+	build: {
+		target: "esnext",
+	},
+	publicDir: "public",
 });
